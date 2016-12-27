5d25787fa325be64c06a74e7eef1de339d6e6ab6
ningyu <ningyu@pxene.com>
2016-12-27 14:03:42 +0800

    添加App数据（小时）CRUD操作。
    Signed-off-by: ningyu <ningyu@pxene.com>

---
+++ src/main/java/com/pxene/pap/service/AppDataHourService.java
@@ -0,0 +1,115 @@
+package com.pxene.pap.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+import com.pxene.pap.domain.beans.AppDataHourBean;
+import com.pxene.pap.domain.model.basic.AppDataHourModel;
+import com.pxene.pap.domain.model.basic.AppDataHourModelExample;
+import com.pxene.pap.exception.DuplicateEntityException;
+import com.pxene.pap.exception.ResourceNotFoundException;
+import com.pxene.pap.repository.basic.AppDataHourDao;
+
+@Service
+public class AppDataHourService extends BaseService
+{
+    @Autowired
+    private AppDataHourDao appDataHourDao;
+    
+    
+    @Transactional
+    public void saveAppDataHour(AppDataHourBean appDataDayBean)
+    {
+        AppDataHourModel appDataDayModel = modelMapper.map(appDataDayBean, AppDataHourModel.class);
+        
+        try
+        {
+            appDataHourDao.insertSelective(appDataDayModel);
+        }
+        catch (DuplicateKeyException exception)
+        {
+            // 违反数据库唯一约束时，向上抛出自定义异常，交给全局异常处理器处理
+            throw new DuplicateEntityException();
+        }
+        
+        // 将DAO创建的新对象复制回传输对象中
+        BeanUtils.copyProperties(appDataDayModel, appDataDayBean);
+        
+    }
+
+    @Transactional
+    public void updateAppDataHour(Integer id, AppDataHourBean appDataDayBean)
+    {
+        // 操作前先查询一次数据库，判断指定的资源是否存在
+        AppDataHourModel appDataDayInDB = appDataHourDao.selectByPrimaryKey(id);
+        if (appDataDayInDB == null)
+        {
+            throw new ResourceNotFoundException();
+        }
+        
+        // 将传输对象映射成数据库Model
+        AppDataHourModel appDataDayModel = modelMapper.map(appDataDayBean, AppDataHourModel.class);
+        appDataDayModel.setId(id);
+        
+        try
+        {
+            appDataHourDao.updateByPrimaryKey(appDataDayModel);
+        }
+        catch (DuplicateKeyException exception)
+        {
+            // 违反数据库唯一约束时，向上抛出自定义异常，交给全局异常处理器处理
+            throw new DuplicateEntityException();
+        }
+        
+        // 将DAO编辑后的新对象复制回传输对象中
+        BeanUtils.copyProperties(appDataHourDao.selectByPrimaryKey(id), appDataDayBean);
+    }
+
+    
+    @Transactional
+    public AppDataHourBean findAppDataHourById(Integer id)
+    {
+        AppDataHourModel appDataDayModel = appDataHourDao.selectByPrimaryKey(id);
+        
+        if (appDataDayModel == null)
+        {
+            throw new ResourceNotFoundException();
+        }
+        
+        // 将DAO创建的新对象复制回传输对象中
+        return modelMapper.map(appDataDayModel, AppDataHourBean.class);
+    }
+
+    
+    @Transactional
+    public List<AppDataHourBean> listAppDataHour()
+    {
+        AppDataHourModelExample example = new AppDataHourModelExample();
+        
+        List<AppDataHourModel> appDataDayModels = appDataHourDao.selectByExample(example);
+        List<AppDataHourBean> appDataDayList = new ArrayList<AppDataHourBean>();
+        
+        if (appDataDayModels == null || appDataDayModels.size() <= 0)
+        {
+            throw new ResourceNotFoundException();
+        }
+        else
+        {
+            // 遍历数据库中查询到的全部结果，逐个将DAO创建的新对象复制回传输对象中
+            for (AppDataHourModel appDataDayModel : appDataDayModels)
+            {
+                appDataDayList.add(modelMapper.map(appDataDayModel, AppDataHourBean.class));
+            }
+        }
+        
+        return appDataDayList;
+    }
+    
+}
---
+++ src/main/java/com/pxene/pap/web/controller/AppDataHourController.java
@@ -0,0 +1,106 @@
+package com.pxene.pap.web.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pxene.pap.common.ResponseUtils;
+import com.pxene.pap.domain.beans.AppDataHourBean;
+import com.pxene.pap.domain.model.custom.PaginationResult;
+import com.pxene.pap.service.AppDataHourService;
+
+/**
+ * APP数据-天
+ * @author ningyu
+ */
+@Controller
+public class AppDataHourController
+{
+    @Autowired
+    private AppDataHourService appDataHourService;
+    
+    
+    /**
+     * 添加App数据。
+     * @param appDataHour 按小时计App数据对象    
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/appDataHour", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public ResponseEntity<Void> addAppDataHour(@Valid @RequestBody AppDataHourBean appDataHour, HttpServletResponse response) throws Exception
+    {
+        appDataHourService.saveAppDataHour(appDataHour);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+    
+    
+    /**
+     * 根据ID编辑指定的App数据（全部更新）。
+     * @param id        App数据ID
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/appDataHour/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public void updateAppDataHour(@PathVariable Integer id, @Valid @RequestBody AppDataHourBean appDataHour, HttpServletResponse response) throws Exception
+    {
+        appDataHourService.updateAppDataHour(id, appDataHour);
+        response.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+    
+    
+    /**
+     * 根据ID查询指定的App数据。
+     * @param id        App数据ID
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/appDataHour/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String getAppDataHour(@PathVariable Integer id, HttpServletResponse response) throws Exception
+    {
+        AppDataHourBean appDataHour = appDataHourService.findAppDataHourById(id);
+        
+        return ResponseUtils.sendReponse(HttpStatus.OK.value(), appDataHour, response);
+    }
+    
+    
+    /**
+     * 列出App数据。
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/appDataHour", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String listAppDataHour(@RequestParam(required = false) Integer pageNo, @RequestParam(required = false) Integer pageSize, HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        Page<Object> pager = null;
+        if (pageNo != null && pageSize != null)
+        {
+            pager = PageHelper.startPage(pageNo, pageSize);
+        }
+        
+        List<AppDataHourBean> appDataHourList = appDataHourService.listAppDataHour();
+        
+        PaginationResult result = new PaginationResult(appDataHourList, pager);
+        return ResponseUtils.sendReponse(HttpStatus.OK.value(), result, response);
+    }
+}
