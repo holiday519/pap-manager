ee50e0c335b68d1a21ae84bb74c745f4120732a0
ningyu <ningyu@pxene.com>
2016-12-27 15:53:45 +0800

    MBG操作添加：MySQL表pap_t_app中缺少字段adx_id、type字段；多出一个type字段。
    Signed-off-by: ningyu <ningyu@pxene.com>

---
+++ src/main/java/com/pxene/pap/domain/model/basic/AppModel.java
@@ -0,0 +1,81 @@
+package com.pxene.pap.domain.model.basic;
+
+import java.util.Date;
+
+public class AppModel extends AppModelKey {
+    private String name;
+
+    private String type;
+
+    private String pkgName;
+
+    private String remark;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getPkgName() {
+        return pkgName;
+    }
+
+    public void setPkgName(String pkgName) {
+        this.pkgName = pkgName == null ? null : pkgName.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", name=").append(name);
+        sb.append(", type=").append(type);
+        sb.append(", pkgName=").append(pkgName);
+        sb.append(", remark=").append(remark);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
