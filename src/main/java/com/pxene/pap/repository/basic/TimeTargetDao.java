d76973bf06ff6d35eb508add5464ee4708bf2468
yuweichao <yuweichao@pxene.com>
2017-01-09 10:10:16 +0800

    提交丢失文件

--- src/main/java/com/pxene/pap/repository/basic/TimeTargetDao.java
+++ src/main/java/com/pxene/pap/repository/basic/TimeTargetDao.java
@@ -2,9 +2,7 @@ package com.pxene.pap.repository.basic;
 
 import com.pxene.pap.domain.models.TimeTargetModel;
 import com.pxene.pap.domain.models.TimeTargetModelExample;
-
 import java.util.List;
-
 import org.apache.ibatis.annotations.Param;
 
 public interface TimeTargetDao {
