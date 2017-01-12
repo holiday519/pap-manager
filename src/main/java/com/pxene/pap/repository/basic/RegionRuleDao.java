6ad9638cd9195522b79ac4262b5c811162800e1d
yuweichao <yuweichao@pxene.com>
2017-01-12 16:48:59 +0800

    上传缺失文件

---
+++ src/main/java/com/pxene/pap/repository/basic/RegionRuleDao.java
@@ -0,0 +1,30 @@
+package com.pxene.pap.repository.basic;
+
+import com.pxene.pap.domain.models.RegionRuleModel;
+import com.pxene.pap.domain.models.RegionRuleModelExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface RegionRuleDao {
+    long countByExample(RegionRuleModelExample example);
+
+    int deleteByExample(RegionRuleModelExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(RegionRuleModel record);
+
+    int insertSelective(RegionRuleModel record);
+
+    List<RegionRuleModel> selectByExample(RegionRuleModelExample example);
+
+    RegionRuleModel selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") RegionRuleModel record, @Param("example") RegionRuleModelExample example);
+
+    int updateByExample(@Param("record") RegionRuleModel record, @Param("example") RegionRuleModelExample example);
+
+    int updateByPrimaryKeySelective(RegionRuleModel record);
+
+    int updateByPrimaryKey(RegionRuleModel record);
+}
---
+++ src/main/java/com/pxene/pap/repository/basic/TimeRuleDao.java
@@ -0,0 +1,30 @@
+package com.pxene.pap.repository.basic;
+
+import com.pxene.pap.domain.models.TimeRuleModel;
+import com.pxene.pap.domain.models.TimeRuleModelExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TimeRuleDao {
+    long countByExample(TimeRuleModelExample example);
+
+    int deleteByExample(TimeRuleModelExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(TimeRuleModel record);
+
+    int insertSelective(TimeRuleModel record);
+
+    List<TimeRuleModel> selectByExample(TimeRuleModelExample example);
+
+    TimeRuleModel selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") TimeRuleModel record, @Param("example") TimeRuleModelExample example);
+
+    int updateByExample(@Param("record") TimeRuleModel record, @Param("example") TimeRuleModelExample example);
+
+    int updateByPrimaryKeySelective(TimeRuleModel record);
+
+    int updateByPrimaryKey(TimeRuleModel record);
+}
