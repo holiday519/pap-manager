89ad106cc8847404e73fd18a7cf9b0dfab01d648
yuweichao <yuweichao@pxene.com>
2017-01-20 17:49:00 +0800

    修改城市省份查询

--- src/main/java/com/pxene/pap/domain/beans/RegionBean.java
+++ src/main/java/com/pxene/pap/domain/beans/RegionBean.java
@@ -1,17 +1,73 @@
 package com.pxene.pap.domain.beans;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+
 /**
  * 地域码表
+ */
+/**
+ * @author yuweichao
+ *
  */
 public class RegionBean {
 	private String id;
 
-    private String name;
+	private String name;
 
-    private BigDecimal longitude;
+	private BigDecimal longitude;
 
-    private BigDecimal latitude;
+	private BigDecimal latitude;
+
+	private City[] citys;
+
+	public static class City {
+		private String id;
+
+		private String name;
+
+		private BigDecimal longitude;
+
+		private BigDecimal latitude;
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public BigDecimal getLongitude() {
+			return longitude;
+		}
+
+		public void setLongitude(BigDecimal longitude) {
+			this.longitude = longitude;
+		}
+
+		public BigDecimal getLatitude() {
+			return latitude;
+		}
+
+		public void setLatitude(BigDecimal latitude) {
+			this.latitude = latitude;
+		}
+
+		@Override
+		public String toString() {
+			return "City [id=" + id + ", name=" + name + ", longitude="
+					+ longitude + ", latitude=" + latitude + "]";
+		}
+	}
 
 	public String getId() {
 		return id;
@@ -45,10 +101,18 @@ public class RegionBean {
 		this.latitude = latitude;
 	}
 
+	public City[] getCitys() {
+		return citys;
+	}
+
+	public void setCitys(City[] citys) {
+		this.citys = citys;
+	}
+
 	@Override
 	public String toString() {
 		return "RegionBean [id=" + id + ", name=" + name + ", longitude="
-				+ longitude + ", latitude=" + latitude + "]";
+				+ longitude + ", latitude=" + latitude + ", citys="
+				+ Arrays.toString(citys) + "]";
 	}
-
 }
--- src/main/java/com/pxene/pap/service/RegionService.java
+++ src/main/java/com/pxene/pap/service/RegionService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import com.pxene.pap.domain.beans.RegionBean;
+import com.pxene.pap.domain.beans.RegionBean.City;
 import com.pxene.pap.domain.models.RegionModel;
 import com.pxene.pap.domain.models.RegionModelExample;
 import com.pxene.pap.exception.ResourceNotFoundException;
@@ -43,6 +44,38 @@ public class RegionService extends BaseService {
 			result.add(regionBean);
 		}
 		
-		return result;
+		List<RegionBean> province = new ArrayList<RegionBean>();
+		List<RegionBean> city = new ArrayList<RegionBean>();
+		for (RegionBean model : result) {
+			if (!"000000".equals(model.getId())) {
+				// 以“0000”结尾的是省
+				if (model.getId().endsWith("0000")) {
+					province.add(model);
+				} else {
+					// 除了省份就是城市
+					city.add(model);
+				}
+			}
+		}
+		for (RegionBean pro : province) {
+			List<RegionBean> cityList = new ArrayList<RegionBean>();
+			for (RegionBean ci : city) {
+				String pStr = pro.getId().substring(0, 4) + "00";
+				String cStr = ci.getId().substring(0, 2) + "0000";
+				if (pStr.equals(cStr)) {
+					cityList.add(ci);
+				}
+			}
+			City[] citys = new City[cityList.size()];
+			for (int i = 0; i < cityList.size(); i++) {
+				citys[i] = modelMapper.map(cityList.get(i), City.class);
+			}
+			if (citys.length > 0) {
+				pro.setCitys(citys);
+			}
+		}
+		
+		return province;
 	}
+	
 }
