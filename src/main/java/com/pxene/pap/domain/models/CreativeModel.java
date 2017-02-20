403ea241e9685381c01bc3d5914ec064461574f4
zhengyi <zhengyi19870519@gmail.com>
2017-02-20 17:30:15 +0800

    提交有错误，不要回退到这

---
+++ src/main/java/com/pxene/pap/domain/models/CreativeModel.java
@@ -0,0 +1,114 @@
+package com.pxene.pap.domain.models;
+
+import java.util.Date;
+
+public class CreativeModel {
+    private String id;
+
+    private String name;
+
+    private String campaignId;
+
+    private String materialId;
+
+    private String tmplId;
+
+    private String type;
+
+    private Float price;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(String campaignId) {
+        this.campaignId = campaignId == null ? null : campaignId.trim();
+    }
+
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId == null ? null : materialId.trim();
+    }
+
+    public String getTmplId() {
+        return tmplId;
+    }
+
+    public void setTmplId(String tmplId) {
+        this.tmplId = tmplId == null ? null : tmplId.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", campaignId=").append(campaignId);
+        sb.append(", materialId=").append(materialId);
+        sb.append(", tmplId=").append(tmplId);
+        sb.append(", type=").append(type);
+        sb.append(", price=").append(price);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
