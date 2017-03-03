60259dd819bd372069b8d2417dfc2eab4ba59b04
ningyu <ningyu@pxene.com>
2017-03-03 19:54:04 +0800

    支持多个Redis数据源：核心代码位于RedisHelper.java中。 使用方法为：   修改redis.properties文件，添加固定前缀的配置项，如redis.primary.ip=192.168.3.91或redis.secondary.ip=192.168.3.91；   在需要使用的类中添加一个私有成员变量：private RedisHelper redisUtils;   在构建方法中进行实例化：redisUtils = RedisHelper.open("redis.primary.");   使用该实例的set或get方法：redisUtils.set(token.getUserid(), tokenStr);。
    Signed-off-by: ningyu <ningyu@pxene.com>

---
+++ src/main/java/com/pxene/pap/common/RedisHelper.java
@@ -0,0 +1,391 @@
+package com.pxene.pap.common;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.pxene.pap.domain.configs.JRedisPoolConfig;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+@Component
+public class RedisHelper
+{
+    private JedisPool jedisPool;
+ 
+    // 缓存生存时间 
+    private static final int expire = 60000;
+    
+   
+    private RedisHelper()
+    {
+    }
+    
+    
+    public static RedisHelper open(String prefix)
+    {
+        RedisHelper redisUtils = new RedisHelper();
+        
+        redisUtils.getJedisPool(prefix);
+        
+        return redisUtils;
+    }
+    
+    /**
+     * 读取配置文件，建立连接池。
+     * @param prefix    redis.properties配置文件前缀
+     */
+    private void getJedisPool(String prefix)
+    {
+        Properties props = new Properties();
+        try
+        {
+            props.load(JRedisPoolConfig.class.getResourceAsStream("/redis.properties"));
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+        String ip = props.getProperty(prefix + "ip");
+        int port = Integer.parseInt(props.getProperty(prefix + "port"));
+        String password = props.getProperty(prefix + "password");
+        int maxActive = Integer.parseInt(props.getProperty(prefix + "pool.maxActive"));
+        int maxIdle = Integer.parseInt(props.getProperty(prefix + "pool.maxIdle"));
+        long maxWait = Integer.parseInt(props.getProperty(prefix + "pool.maxWait"));
+        boolean testOnBorrow = Boolean.parseBoolean(props.getProperty(prefix + "pool.testOnBorrow"));
+        boolean testOnReturn = Boolean.parseBoolean(props.getProperty(prefix + "pool.testOnReturn"));
+        
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxActive);
+        config.setMaxIdle(maxIdle);
+        config.setMaxWaitMillis(maxWait);
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(testOnReturn);
+        
+        // redis如果设置了密码：
+        if (!StringUtils.isEmpty(password))
+        {
+            jedisPool = new JedisPool(config, ip, port, 10000, password);
+        }
+        else
+        {
+            jedisPool = new JedisPool(config, ip, port, 10000);
+        }
+    }
+    
+    /**
+     * 从jedis连接池中获取获取jedis对象
+     */
+    public Jedis getJedis()
+    {
+        return jedisPool.getResource();
+    }
+    
+    /**
+     * 回收jedis
+     */
+    public void close(Jedis jedis)
+    {
+        jedis.close();
+    }
+    
+    /**
+     * 设置过期时间
+     */
+    public void expire(String key, int seconds)
+    {
+        if (seconds <= 0)
+        {
+            return;
+        }
+        Jedis jedis = getJedis();
+        jedis.expire(key, seconds);
+        close(jedis);
+    }
+    
+    /**
+     * 设置默认过期时间
+     */
+    public void expire(String key)
+    {
+        expire(key, expire);
+    }
+    
+    public void set(String key, String value)
+    {
+        if (isBlank(key))
+        {
+            return;
+        }
+        Jedis jedis = getJedis();
+        jedis.set(key, value);
+        close(jedis);
+    }
+    
+    public void set(String key, Object value)
+    {
+        if (isBlank(key))
+        {
+            return;
+        }
+        Jedis jedis = getJedis();
+        jedis.set(key.getBytes(), SerializeUtils.serialize(value));
+        close(jedis);
+    }
+    
+    public void set(String key, int value)
+    {
+        if (isBlank(key))
+        {
+            return;
+        }
+        set(key, String.valueOf(value));
+    }
+    
+    public void set(String key, long value)
+    {
+        if (isBlank(key))
+        {
+            return;
+        }
+        set(key, String.valueOf(value));
+    }
+    
+    public void set(String key, float value)
+    {
+        if (isBlank(key))
+        {
+            return;
+        }
+        set(key, String.valueOf(value));
+    }
+    
+    public void set(String key, double value)
+    {
+        if (isBlank(key))
+        {
+            return;
+        }
+        set(key, String.valueOf(value));
+    }
+    
+    public Float getFloat(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        return Float.valueOf(getStr(key));
+    }
+    
+    public Double getDouble(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        return Double.valueOf(getStr(key));
+    }
+    
+    public Long getLong(String key)
+    {
+        if (isBlank(key))
+            return null;
+        return Long.valueOf(getStr(key));
+    }
+    
+    public Integer getInt(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        return Integer.valueOf(getStr(key));
+    }
+    
+    public String getStr(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        Jedis jedis = getJedis();
+        String value = jedis.get(key);
+        close(jedis);
+        return value;
+    }
+    
+    public Object getObj(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        Jedis jedis = getJedis();
+        if (jedis.get(key.getBytes()) == null)
+        {
+            return null;
+        }
+        byte[] bits = jedis.get(key.getBytes());
+        Object obj = SerializeUtils.unserialize(bits);
+        close(jedis);
+        return obj;
+    }
+    
+    public void delete(String key)
+    {
+        Jedis jedis = getJedis();
+        if (jedis.get(key) != null)
+        {
+            jedis.del(key);
+        }
+        close(jedis);
+    }
+    
+    public String[] getKeys(String pattern)
+    {
+        if (isBlank(pattern))
+        {
+            return null;
+        }
+        Jedis jedis = getJedis();
+        Set<String> keySet = jedis.keys(pattern);
+        String[] keys = new String[keySet.size()];
+        int index = 0;
+        for (String key : keySet)
+        {
+            keys[index] = key;
+            index++;
+        }
+        close(jedis);
+        return keys;
+    }
+    
+    public void deleteByPattern(String pattern)
+    {
+        Jedis jedis = getJedis();
+        String[] keys = getKeys(pattern);
+        if (keys != null && keys.length != 0)
+        {
+            if (keys.length == 1)
+            {
+                jedis.del(keys[0]);
+            }
+            else
+            {
+                jedis.del(keys);
+            }
+        }
+        close(jedis);
+    }
+    
+    public Set<String> sget(String key)
+    {
+        Jedis jedis = getJedis();
+        close(jedis);
+        return jedis.smembers(key);
+    }
+    
+    public void sset(String key, String... members)
+    {
+        Jedis jedis = getJedis();
+        jedis.sadd(key, members);
+        close(jedis);
+    }
+    
+    public boolean sismember(String key, String member)
+    {
+        Jedis jedis = getJedis();
+        boolean res = jedis.sismember(key, member);
+        close(jedis);
+        return res;
+    }
+    
+    public void sdelete(String key, String... members)
+    {
+        Jedis jedis = getJedis();
+        jedis.srem(key, members);
+        close(jedis);
+    }
+    
+    public void hset(String key, Map<String, String> value)
+    {
+        Jedis jedis = getJedis();
+        jedis.hmset(key, value);
+        close(jedis);
+    }
+    
+    public void hdelete(String key)
+    {
+        Jedis jedis = getJedis();
+        if (jedis.hgetAll(key) != null)
+        {
+            jedis.del(key);
+        }
+        close(jedis);
+    }
+    
+    public Map<String, String> hget(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        Jedis jedis = getJedis();
+        Map<String, String> hgetAll = jedis.hgetAll(key);
+        close(jedis);
+        return hgetAll;
+    }
+    
+    public Set<String> hkeys(String key)
+    {
+        if (isBlank(key))
+        {
+            return null;
+        }
+        Jedis jedis = getJedis();
+        Set<String> hgetAll = jedis.hkeys(key);
+        close(jedis);
+        return hgetAll;
+    }
+    
+    public boolean isBlank(String str)
+    {
+        return str == null || "".equals(str.trim());
+    }
+    
+    public boolean exists(String key)
+    {
+        Jedis jedis = getJedis();
+        boolean isexist = jedis.exists(key);
+        close(jedis);
+        return isexist;
+    }
+    
+    public void sddKey(String key, List<String> values)
+    {
+        if (values != null && !values.isEmpty())
+        {
+            Jedis jedis = getJedis();
+            for (String value : values)
+            {
+                jedis.sadd(key, value);
+            }
+            close(jedis);
+        }
+    }
+}
