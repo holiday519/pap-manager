3331d8c493e99dd4f837835d690a0c6b49e41376
ningyu <ningyu@pxene.com>
2017-03-29 18:36:35 +0800

    添加新功能：支持上传文件到服务器或本地（通过配置文件application-dev.properties中的pap.fileserver.mode属性来切换，remote表示远程、local表示本地）。 本地配置：     pap.fileserver.local.upload.dir=/data/nginx/pap/（本地主机上传根目录）     pap.fileserver.local.url.prefix=http://192.168.3.93/pap/（本地主机HTTP映射路径）
    远程配置：
        pap.fileserver.remote.host=115.182.33.177（远程主机IP）
        pap.fileserver.remote.port=22（远程主机端口）
        pap.fileserver.remote.username=root（远程主机用户名）
        pap.fileserver.remote.password=PowerXene123（远程主机密码）
        pap.fileserver.remote.upload.dir=/home/pxene-pap/（远程主机上传根目录）
        pap.fileserver.remote.url.prefix=http://115.182.33.177/pxene-pap-pap/（远程主机HTTP映射路径）
    
    Signed-off-by: ningyu <ningyu@pxene.com>

---
+++ src/main/java/com/pxene/pap/common/ScpUtils.java
@@ -0,0 +1,182 @@
+package com.pxene.pap.common;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ConnectException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.SCPClient;
+
+public class ScpUtils
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScpUtils.class);
+    
+    private static ScpUtils instance;
+    private static Connection connection;
+    
+    private String ip;
+    private int port;
+    private String username;
+    private String password;
+    
+    public String getUsername()
+    {
+        return username;
+    }
+    
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+    
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    
+    public int getPort()
+    {
+        return port;
+    }
+    
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+    
+    
+    public ScpUtils(String IP, int port, String username, String passward) 
+    {
+        this.ip = IP;
+        this.port = port;
+        this.username = username;
+        this.password = passward;
+    }
+    
+    
+    public static synchronized ScpUtils getInstance(String IP, int port, String username, String passward)
+    {
+        if (instance == null)
+        {
+            doSync(IP, port, username, passward);
+        }
+        return instance;
+    }
+    
+    private synchronized static void doSync(String IP, int port, String username, String passward)
+    {
+        if (instance == null)
+        {
+            instance = new ScpUtils(IP, port, username, passward);
+        }
+    }
+    
+    
+    public void getFile(String remoteFile, String localTargetDirectory) throws IOException
+    {
+        SCPClient client = new SCPClient(connection);
+        client.get(remoteFile, localTargetDirectory);
+    }
+    
+    public void putFile(String localFile, String remoteTargetDirectory) throws IOException
+    {
+        SCPClient client = new SCPClient(connection);
+        client.put(localFile, remoteTargetDirectory);
+    }
+    
+    public void putFile(byte[] data, String remoteFileName, String remoteTargetDirectory) throws IOException
+    {
+        SCPClient client = new SCPClient(connection);
+        client.put(data, remoteFileName, remoteTargetDirectory);
+    }
+    
+    public void putFile(String localFile, String remoteFileName, String remoteTargetDirectory, String mode) throws IOException
+    {
+        SCPClient client = new SCPClient(connection);
+        if ((mode == null) || (mode.length() == 0))
+        {
+            mode = "0600";
+        }
+        client.put(localFile, remoteFileName, remoteTargetDirectory, mode);
+        
+        // 重命名
+        ch.ethz.ssh2.Session sess = connection.openSession();
+        String tmpPathName = remoteTargetDirectory + File.separator + remoteFileName;
+        String newPathName = tmpPathName.substring(0, tmpPathName.lastIndexOf("."));
+        sess.execCommand("mv " + remoteFileName + " " + newPathName);// 重命名回来
+    }
+    
+    public void copy(String from, String to) throws IOException
+    {
+        ch.ethz.ssh2.Session sess = connection.openSession();
+        sess.execCommand("cp " + from + " " + to);
+        sess.close();
+    }
+    
+    public void delete(String path) throws IOException
+    {
+        ch.ethz.ssh2.Session sess = connection.openSession();
+        sess.execCommand("rm -rf " + path);
+        sess.close();
+    }
+
+    public ScpUtils connect() throws Exception
+    {
+        connection = new Connection(ip, port);
+        connection.connect();
+        
+        boolean isAuthenticated = connection.authenticateWithPassword(username, password);
+        if (isAuthenticated == false)
+        {
+            String msg = "authentication failed";
+            LOGGER.error(msg);
+            throw new ConnectException(msg);
+        }
+        return instance;
+    }
+    
+    public void close()
+    {
+        connection.close();
+    }
+    
+    public static byte[] getBytes(String filePath)
+    {
+        byte[] buffer = null;
+        try
+        {
+            File file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file);
+            ByteArrayOutputStream byteArray = new ByteArrayOutputStream(1024 * 1024);
+            byte[] b = new byte[1024 * 1024];
+            int i;
+            while ((i = fis.read(b)) != -1)
+            {
+                byteArray.write(b, 0, i);
+            }
+            fis.close();
+            byteArray.close();
+            buffer = byteArray.toByteArray();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+}
