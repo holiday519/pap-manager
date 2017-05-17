bb2e4cb6d2e0fddc926651bcc288e7685cc2687e
zhengyi <zhengyi19870519@gmail.com>
2017-05-17 12:53:00 +0800

    数据库有个字段拼写有误，改正

---
+++ src/main/java/com/pxene/pap/domain/models/VideoTmplModel.java
@@ -0,0 +1,125 @@
+package com.pxene.pap.domain.models;
+
+import java.util.Date;
+
+public class VideoTmplModel {
+    private String id;
+
+    private Integer width;
+
+    private Integer height;
+
+    private String formats;
+
+    private Float maxVolume;
+
+    private Integer maxTimelength;
+
+    private Integer minTimelength;
+
+    private String imageId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getFormats() {
+        return formats;
+    }
+
+    public void setFormats(String formats) {
+        this.formats = formats == null ? null : formats.trim();
+    }
+
+    public Float getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(Float maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public Integer getMaxTimelength() {
+        return maxTimelength;
+    }
+
+    public void setMaxTimelength(Integer maxTimelength) {
+        this.maxTimelength = maxTimelength;
+    }
+
+    public Integer getMinTimelength() {
+        return minTimelength;
+    }
+
+    public void setMinTimelength(Integer minTimelength) {
+        this.minTimelength = minTimelength;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId == null ? null : imageId.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", width=").append(width);
+        sb.append(", height=").append(height);
+        sb.append(", formats=").append(formats);
+        sb.append(", maxVolume=").append(maxVolume);
+        sb.append(", maxTimelength=").append(maxTimelength);
+        sb.append(", minTimelength=").append(minTimelength);
+        sb.append(", imageId=").append(imageId);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
