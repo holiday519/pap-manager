3d9d3d15447bd4dc4d3bdd70ca16a1475bec9367
zhengyi <zhengyi19870519@gmail.com>
2017-06-09 10:58:10 +0800

    删除无用代码

---
+++ src/main/java/com/pxene/pap/constant/ConfKeyConstant.java
@@ -0,0 +1,28 @@
+package com.pxene.pap.constant;
+
+public class ConfKeyConstant {
+	
+	public static final String FILESERVER_MODE = "pap.fileserver.mode";
+	
+	public static final String FILESERVER_LOCAL_UPLOAD_DIR = "pap.fileserver.local.upload.dir";
+	
+	public static final String FILESERVER_LOCAL_URL_PREFIX = "pap.fileserver.local.url.prefix";
+	
+	public static final String FILESERVER_REMOTE_HOST = "pap.fileserver.remote.host";
+	
+	public static final String FILESERVER_REMOTE_PORT = "pap.fileserver.remote.port";
+	
+	public static final String FILESERVER_REMOTE_USERNAME = "pap.fileserver.remote.username";
+	
+	public static final String FILESERVER_REMOTE_PASSWORD = "pap.fileserver.remote.password";
+	
+	public static final String FILESERVER_REMOTE_UPLOAD_DIR = "pap.fileserver.remote.upload.dir";
+	
+	public static final String FILESERVER_REMOTE_URL_PREFIX = "pap.fileserver.remote.url.prefix";
+	
+	public static final String POPULATION_PATH = "pap.population.path";
+	
+	public static final String EXCEL_DOWNLOAD_MAPURL = "pap.excel.download.mapUrl";
+	
+	public static final String EXCEL_SAVEPATH = "pap.excel.savePath";
+}
