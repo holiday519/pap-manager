3af047d150c7c8c7ab93d8ef94a04af86f1a6488
lizhuoling <lizhuoling@pxene.com>
2017-06-09 14:05:42 +0800

    重新生成规则表相关文件

---
+++ src/main/java/com/pxene/pap/domain/models/RuleModel.java
@@ -0,0 +1,103 @@
+package com.pxene.pap.domain.models;
+
+import java.util.Date;
+
+public class RuleModel {
+    private String id;
+
+    private String name;
+
+    private String conditions;
+
+    private String relation;
+
+    private String staticId;
+
+    private String projectId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(String conditions) {
+        this.conditions = conditions == null ? null : conditions.trim();
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation == null ? null : relation.trim();
+    }
+
+    public String getStaticId() {
+        return staticId;
+    }
+
+    public void setStaticId(String staticId) {
+        this.staticId = staticId == null ? null : staticId.trim();
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId == null ? null : projectId.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", conditions=").append(conditions);
+        sb.append(", relation=").append(relation);
+        sb.append(", staticId=").append(staticId);
+        sb.append(", projectId=").append(projectId);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
