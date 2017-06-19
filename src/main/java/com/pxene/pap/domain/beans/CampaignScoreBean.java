396363c3e078fcf551ef214506ff44b1f0c3ad3a
ningyu <ningyu@pxene.com>
2017-06-19 17:40:38 +0800

    补传评分接口返回用的Bean。
    Signed-off-by: ningyu <ningyu@pxene.com>

---
+++ src/main/java/com/pxene/pap/domain/beans/CampaignScoreBean.java
@@ -0,0 +1,53 @@
+package com.pxene.pap.domain.beans;
+
+import java.util.List;
+import java.util.Map;
+
+public class CampaignScoreBean
+{
+    private double score;
+    private String ruleName;
+    private String ruleTrigger;
+    private List<Map<String, String>> formulaList;
+    
+    
+    public double getScore()
+    {
+        return score;
+    }
+    public void setScore(double score)
+    {
+        this.score = score;
+    }
+    public String getRuleName()
+    {
+        return ruleName;
+    }
+    public void setRuleName(String ruleName)
+    {
+        this.ruleName = ruleName;
+    }
+    public String getRuleTrigger()
+    {
+        return ruleTrigger;
+    }
+    public void setRuleTrigger(String ruleTrigger)
+    {
+        this.ruleTrigger = ruleTrigger;
+    }
+    public List<Map<String, String>> getFormulaList()
+    {
+        return formulaList;
+    }
+    public void setFormulaList(List<Map<String, String>> formulaList)
+    {
+        this.formulaList = formulaList;
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        return "CampaignScoreBean [score=" + score + ", ruleName=" + ruleName + ", ruleTrigger=" + ruleTrigger + ", formulaList=" + formulaList + "]";
+    }
+}
